84e20eaaee99eedf353de7f06504e28da9363f3a
Jose Ugal <joseugal17@icloud.com>
2022-12-16 06:29:33 +0100

    chore(Root): Base startup

---
+++ src/apps/moas/backend/start.ts
@@ -0,0 +1,13 @@
+import { MoasBackendApp } from './MoasBackendApp';
+
+try {
+  new MoasBackendApp().start();
+} catch (e) {
+  console.log(e);
+  process.exit(1);
+}
+
+process.on('uncaughtException', err => {
+  console.log('uncaughtException', err);
+  process.exit(1);
+});
